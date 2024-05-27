346a01ba017d7752ba013f380f4d5eb332b175ad
hoangfitus <ndhoang22@clc.fitus.edu.vn>
2024-05-27 20:42:03 +0700

    build: update frontend dependencies and configuration files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                kodchasan: ['Kodchasan', 'sans-serif'],
+                italianno: ['Italianno', 'cursive'],
+                montserrat: ['Montserrat', 'sans-serif'],
+            },
+            backgroundImage: {
+                'grad-pattern': "url('./assets/img/background.svg')",
+            },
+        },
     },
     plugins: [],
 };
