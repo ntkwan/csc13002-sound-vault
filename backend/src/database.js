2ac41029057c591bf4929ab805880c2613c678d9
nkwn <npquan2704@gmail.com>
2024-05-28 23:09:49 +0700

    build: add database connection and error handling

---
+++ backend/src/database.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+async function connect_database() {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Database is connected`);
+    } catch (error) {
+        console.error('Error: ${error.message}');
+    }
+}
+
+module.exports = { connect_database };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,42 @@
-const dotenv = require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const morgan = require('morgan');
 const express = require('express');
+const cors = require('cors');
+
+const databaseModule = require('./database');
+const errorLogging = require('http-errors');
 
 const port = process.env.PORT;
 const app = express();
 
+databaseModule.connect_database();
+app.use(cors({
+    origin: process.env.CLIENT_URI,
+    credentials: true
+}
+));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// HTTP method logging
 app.use(morgan('combined'));
 
+//Routes
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.use((req, res, next) => {
+    next(errorLogging.NotFound('This page does not exists'));
+});
+
+app.use((err, req, res, next) => {
+    res.json({
+        status: err.status || 500,
+        message: err.message
+    });
 });
 
 app.listen(port, () => {
