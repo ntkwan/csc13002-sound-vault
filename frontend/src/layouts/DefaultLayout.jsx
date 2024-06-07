3cd4878cb157650f66d3358481065249ce6deb1a
Npeka <128128732+Npeka@users.noreply.github.com>
2024-06-07 11:12:11 +0700

    refactor: update DefaultLayout and scrollbar

--- frontend/src/layouts/DefaultLayout.jsx
+++ frontend/src/layouts/DefaultLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import { Header, Sidebar } from "@components";
 
 function DefaultLayout() {
   return (
     <>
       Default Layout
-      <main>
+      <Header />
+      <Sidebar />
+      <main className="relative px-10 mt-[70px] ml-[175px] ">
         <Outlet />
       </main>
     </>
