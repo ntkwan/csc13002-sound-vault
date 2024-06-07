98c3c5cc2cda0698c8e8cb27d6c008e9ff49a91a
Npeka <128128732+Npeka@users.noreply.github.com>
2024-06-08 00:49:09 +0700

    feat: add ArtistPage

--- frontend/src/pages/ArtistPage.jsx
+++ frontend/src/pages/ArtistPage.jsx
@@ -1,7 +1,58 @@
+import MediaDisplay from "@components/MediaDisplay";
+
 function ArtistPage() {
+	const mediaData = [
+		{
+			type: "artist",
+			header: "Popular Artists",
+			visibility: "",
+			link: "",
+			data: [
+				{ name: "Hoàng Thùy Linh", desc: "artist" },
+				{ name: "Phan Mạnh Quỳnh", desc: "artist" },
+				{ name: "Obito", desc: "artist" },
+				{ name: "HIEUTHUHAI", desc: "artist" },
+				{ name: "Anh Tú", desc: "artist", ext: "png" },
+				{ name: "Sơn Tùng MTP", desc: "artist" },
+			],
+		},
+		{
+			type: "artist",
+			header: "Indie Artists",
+			visibility: "",
+			link: "",
+			data: [
+				{ name: "Hoàng Thùy Linh", desc: "artist" },
+				{ name: "Phan Mạnh Quỳnh", desc: "artist" },
+				{ name: "Obito", desc: "artist" },
+				{ name: "HIEUTHUHAI", desc: "artist" },
+				{ name: "Anh Tú", desc: "artist", ext: "png" },
+				{ name: "Sơn Tùng MTP", desc: "artist" },
+			],
+		},
+		{
+			type: "artist",
+			header: "New Artists",
+			visibility: "",
+			link: "",
+			data: [
+				{ name: "Hoàng Thùy Linh", desc: "artist" },
+				{ name: "Phan Mạnh Quỳnh", desc: "artist" },
+				{ name: "Obito", desc: "artist" },
+				{ name: "HIEUTHUHAI", desc: "artist" },
+				{ name: "Anh Tú", desc: "artist", ext: "png" },
+				{ name: "Sơn Tùng MTP", desc: "artist" },
+			],
+		},
+	];
+
 	return (
-		<div>
-			<h1>Artist Page</h1>
+		<div className="Media__container grid auto-rows-auto gap-y-14 mt-20 font-kodchasan">
+			{mediaData.map((media, index) => (
+				<MediaDisplay
+					key={index} media={media} displayItems="2"
+					displayType="grid grid-cols-6" />
+			))}
 		</div>
 	);
 };
