a14631ff1b6168fb778f6c3965ece6a9d2917837
nkwn <npquan2704@gmail.com>
2024-06-07 22:29:50 +0700

    feat: add forgot-password email authentication

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,5 +7,6 @@ router.post('/signin', authorize.signin);
 router.post('/signup', authorize.signup);
 router.post('/signout', authorize.signout);
 router.post('/reset-password/:email/:token', authorize.reset_password);
+router.post('/forgot-password', authorize.forgot_password);
 
 module.exports = router;
