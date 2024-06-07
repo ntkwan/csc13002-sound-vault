6dda109e1e65611adb646b3a64e03556ef8a8158
Npeka <128128732+Npeka@users.noreply.github.com>
2024-06-07 23:21:08 +0700

    feat: add ProfilePage
    Update MediaDisplay.jsx

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,4 +1,5 @@
 export { default as HomePage } from "./HomePage";
+export { default as ProfilePage } from "./ProfilePage";
 export { default as SignInPage } from "./LoginPage";
 export { default as SignUpPage } from "./SignUpPage";
 export { default as ForgotPassPage } from "./ForgotPassPage";
