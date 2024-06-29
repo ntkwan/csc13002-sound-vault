0a369ab91b0bb61157f7daf236db2a2acbadf54e
hoangfitus <ndhoang22@clc.fitus.edu.vn>
2024-06-29 16:59:00 +0700

    hotfix: Fix error syntax

--- frontend/src/store/store.jsx
+++ frontend/src/store/store.jsx
@@ -1,40 +1,47 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import { api } from "@services/api";
-import { authReducer } from "@features/authentication/slices";
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { api } from '@services/api';
+import { authReducer } from '@features/authentication/slices';
 
 const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["auth"],
+    key: 'root',
+    storage,
+    whitelist: ['auth'],
 };
 
 const rootReducer = combineReducers({
-  [api.reducerPath]: api.reducer,
-  auth: authReducer,
+    [api.reducerPath]: api.reducer,
+    auth: authReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }).concat(api.middleware),
-  devTools: true,
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }).concat(api.middleware),
+    devTools: true,
 });
 
 export let persistor = persistStore(store);
