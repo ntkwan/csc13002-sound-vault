b79592d7181a522327dfbc80d1a9e25d8e866481
Npeka <128128732+Npeka@users.noreply.github.com>
2024-07-08 23:44:33 +0700

    refactor: set the state of sidebar for the guest

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -10,85 +10,92 @@ import PropTypes from 'prop-types';
 function Sidebar() {
     const token = useSelector(selectCurrentToken);
     const userProfile = useSelector(selectUserProfile);
-    const { isAdmin } = userProfile;
+    const isAdmin = userProfile?.isAdmin;
+
     return (
         <aside className="sidebar fixed bottom-[70px] top-[70px] z-10 w-max select-none space-y-10 pt-10 text-xs uppercase backdrop-blur-md">
             <section className="ml-2">
                 <span className="ml-3">Browse</span>
-                {token && isAdmin ? (
-                    <>
-                        <SidebarLink
-                            to="admin/user"
-                            icon={ProfileIcon}
-                            label="users"
-                        />
-                        <SidebarLink
-                            to="admin/song"
-                            icon={MusicIcon}
-                            label="songs"
-                        />
-                        <SidebarLink
-                            to="admin/report"
-                            icon={EmailIcon}
-                            label="review report"
-                        />
-                    </>
-                ) : (
-                    <>
-                        <SidebarLink to="" icon={HomeIcon} label="home" />
-                        <SidebarLink
-                            to="profile"
-                            icon={ProfileIcon}
-                            label="profile"
-                        />
-                        <SidebarLink
-                            to="library"
-                            icon={MusicIcon}
-                            label="library"
-                        />
-                        <SidebarLink
-                            to="artist"
-                            icon={HeadphoneIcon}
-                            label="artists"
-                        />
-                    </>
-                )}
+                <div>
+                    <SidebarLink
+                        to=""
+                        icon={HomeIcon}
+                        label="home"
+                    />
+                    {token ? (
+                        isAdmin ? (
+                            <>
+                                <SidebarLink
+                                    to="admin/user"
+                                    icon={ProfileIcon}
+                                    label="users"
+                                />
+                                <SidebarLink
+                                    to="admin/song"
+                                    icon={MusicIcon}
+                                    label="songs"
+                                />
+                                <SidebarLink
+                                    to="admin/report"
+                                    icon={EmailIcon}
+                                    label="review report"
+                                />
+                            </>
+                        ) : (
+                            <>
+                                <SidebarLink
+                                    to="profile"
+                                    icon={ProfileIcon}
+                                    label="profile"
+                                />
+                                <SidebarLink
+                                    to="library"
+                                    icon={MusicIcon}
+                                    label="library"
+                                />
+                            </>
+                        )
+                    ) : null}
+                    <SidebarLink
+                        to="artist"
+                        icon={HeadphoneIcon}
+                        label="artists"
+                    />
+                </div>
             </section>
             <section className="ml-2">
                 <span className="ml-3">Help</span>
-                {token && isAdmin ? (
-                    <></>
-                ) : (
-                    <>
-                        <SidebarLink
-                            to="aboutus"
-                            icon={EmailIcon}
-                            label="About Us"
-                        />
-                        <SidebarLink
-                            to="term&policy"
-                            icon={InfoIcon}
-                            label="Terms & Policy"
-                        />
-                    </>
-                )}
-                <SidebarLink to="setting" icon={SettingIcon} label="setting" />
-                {token && <Logout />}
+                <div>
+                    {!isAdmin && (
+                        <>
+                            <SidebarLink
+                                to="aboutus"
+                                icon={EmailIcon}
+                                label="About Us"
+                            />
+                            <SidebarLink
+                                to="term&policy"
+                                icon={InfoIcon}
+                                label="Terms & Policy"
+                            />
+                        </>
+                    )}
+                    <SidebarLink to="setting" icon={SettingIcon} label="setting" />
+                    {token && <Logout />}
+                </div>
             </section>
         </aside>
     );
 }
 
-export default Sidebar;
-
 SidebarLink.propTypes = {
-    to: PropTypes.string,
+    to: PropTypes.string.isRequired,
     icon: PropTypes.func,
-    label: PropTypes.string,
+    label: PropTypes.string.isRequired,
     children: PropTypes.node,
 };
 
-function SidebarLink({ to, icon, label, children }) {
+function SidebarLink({ to, icon: Icon, label, children }) {
     return (
         <NavLink
             to={to}
@@ -96,9 +103,11 @@ function SidebarLink({ to, icon, label, children }) {
                 `relative my-1 flex items-center rounded-full px-3 py-3 leading-none transition-colors duration-400 ease-in-out ${isActive ? 'bg-white bg-opacity-20' : 'hover:bg-white hover:bg-opacity-20'}`
             }
         >
-            {icon && icon()}
+            {Icon && <Icon />}
             <span className="pl-3">{label}</span>
             {children}
         </NavLink>
     );
 }
+
+export default Sidebar;
