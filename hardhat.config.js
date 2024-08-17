aaa4ccc4a320504cce178a6683720889ba124230
nkwn <npquan2704@gmail.com>
2024-08-18 00:04:33 +0700

    refactor: deploy smc on testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,21 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require('@nomicfoundation/hardhat-toolbox');
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const { vars } = require('hardhat/config');
+const INFURA_API_KEY = vars.get(process.env.API_KEY);
+const SEPOLIA_PRIVATE_KEY = vars.get(process.env.PRIVATE_KEY);
+
 module.exports = {
-  solidity: "0.8.24",
-  networks: {
-    hardhat: {
-      chainId: 1337,
+    solidity: '0.8.24',
+    networks: {
+        sepolia: {
+            url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+            accounts: [SEPOLIA_PRIVATE_KEY],
+        },
     },
-  },
-  paths: {
-    artifacts: "./artifacts",
-  },
+    paths: {
+        artifacts: './artifacts',
+    },
 };
