fe06e796cac057acdc5cf83a548afe94b46cf97b
Npeka <128128732+Npeka@users.noreply.github.com>
2024-08-28 02:41:52 +0700

    refactor: fix loading icon

--- frontend/src/pages/ProfilePageUploadMusic.jsx
+++ frontend/src/pages/ProfilePageUploadMusic.jsx
@@ -113,6 +113,28 @@ function ProfilePageUploadMusic() {
         }));
     }
 
+    const iconUpdate = (
+        <svg
+            className="mr-4 min-h-5 min-w-5 animate-spin text-blue-500"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+            ></circle>
+            <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+        </svg>
+    );
+
     return (
         <>
             <div className="upload-music__page pt-8">
@@ -293,31 +315,17 @@ function ProfilePageUploadMusic() {
                             </p>
                         </div>
                         <button
-                            className="relative m-auto flex min-w-24 max-w-32 select-none items-center justify-center rounded-full border-2 px-5 py-3"
+                            className="relative m-auto flex min-w-36 max-w-36 select-none items-center justify-center rounded-full border-2 px-5 py-3"
                             type="submit"
                         >
-                            {isLoadingSubmitMusic && (
-                                <svg
-                                    className="mr-4 h-6 w-6 animate-spin text-blue-500"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <circle
-                                        className="opacity-25"
-                                        cx="12"
-                                        cy="12"
-                                        r="10"
-                                        stroke="currentColor"
-                                        strokeWidth="4"
-                                    ></circle>
-                                    <path
-                                        className="opacity-75"
-                                        fill="currentColor"
-                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                    ></path>
-                                </svg>
+                            {isLoadingSubmitMusic && iconUpdate}
+                            {isLoadingSubmitMusic ? (
+                                <span className="-translate-x-[7px]">
+                                    Updating...
+                                </span>
+                            ) : (
+                                'Update'
                             )}
-                            {isLoadingSubmitMusic ? 'Uploading...' : 'Upload'}
                         </button>
                     </div>
                 </form>
